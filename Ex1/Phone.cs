c55723c0d3c9b87760819237365a70ab97dc41b2
Quynh anh <71160175+Quynhanh4622@users.noreply.github.com>
2021-06-10 10:39:24 +0700

    update

---
+++ Ex1/Phone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex1
+{
+    public class Phone
+    {
+        public string PhoneName { get; set; }
+        public string PhoneType { get; set; }
+        public float PhonePrice { get; set; }
+
+        public void display()
+        {
+            Console.WriteLine($"Name: {PhoneName}, type:  {PhoneType}, price: {PhonePrice}");
+        }
+    }
+}
